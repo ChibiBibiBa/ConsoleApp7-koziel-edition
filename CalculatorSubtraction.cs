e7e2b6727b42c75eea865922dc6e523e32338832
ChibiBibiBa <marcinstanisz43@interia.pl>
2024-12-10 09:36:10 +0100

    Subtraction

---
+++ CalculatorSubtraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class CalculatorSubtraction
+    {
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
